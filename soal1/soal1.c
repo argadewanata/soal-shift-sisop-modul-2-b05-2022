39884d2089593b0ae308aec1d92f03e54cb77caf
Miawmiw <fahmimuhazir.205025@mhs.its.ac.id>
2022-03-19 14:39:56 +0000

    Updating file for number 1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -8,3 +8,5 @@ int Primogems = 79000;
 char *url_Char = "https://drive.google.com/u/0/uc?id=1xYYmsslb-9s8-4BDvosym7R4EmPi6BHp&export=download";
 char *url_Wp = "https://drive.google.com/u/0/uc?id=1XSkAqqjkNmzZ0AdIZQt_eWGOZ0eJyNlT&export=download";
 
+
+
