68142f8514e49adecefdc97fcde594ca16d2ec61
Miawmiw <fahmimuhazir.205025@mhs.its.ac.id>
2022-03-19 12:29:19 +0000

    Au ah gelap

---
+++ soal1/soal1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <json-c/json.h>
+
+int Primogems = 79000;
+char *url_Char = "https://drive.google.com/u/0/uc?id=1xYYmsslb-9s8-4BDvosym7R4EmPi6BHp&export=download";
+char *url_Wp = "https://drive.google.com/u/0/uc?id=1XSkAqqjkNmzZ0AdIZQt_eWGOZ0eJyNlT&export=download";
+
